94dcd23883f6e07b3ffdb2384182628afe73e4de
HariHaran123G <hariharan3141592@gmail.com>
2026-05-09 13:37:04 +0530

    Fix SQL syntax for total orders and cancellation rate

--- SQL/01-Cancellation-rate.sql
+++ SQL/01-Cancellation-rate.sql
@@ -1,11 +1,11 @@
 WITH BASE AS(
   SELECT o.city, SUM( case when d.status = 'Cancelled' then 1 else 0 END)
                   AS Cancelled_nos,
-  COUNT(*) AS 'Total_orders'
+  COUNT(*) AS Total_orders
   FROM deliveries d
   JOIN orders o
   ON o.order_id=d.order_id
   GROUP BY o.city
   )
-SELECT *, Cancelled_nos*100.0/Total_orders as 'Cancellation_rate'
+SELECT *, Cancelled_nos*100.0/Total_orders as Cancellation_rate
 FROM BASE
